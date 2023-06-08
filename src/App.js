024a2ef11a54757091e28d5a5caeab15b8b66c68
jessaberry <shii@students.ubc.ca>
2023-06-08 13:09:19 -0700

    feat(Experience): added button with dropdown menu for various types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Experience from './components/experience/Experience';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        <Experience />
+      </body>
     </div>
   );
 }
