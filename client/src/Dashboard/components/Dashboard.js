5a83fb5afcc1de99ccdc0c2738949c2c8c019790
parths10 <parth.sehtiyaa@gmail.com>
2023-07-03 14:12:36 -0700

    Add trip triggers popup form

--- client/src/Dashboard/components/Dashboard.js
+++ client/src/Dashboard/components/Dashboard.js
@@ -1,18 +1,35 @@
-import React from "react";
+// import React from "react";
 import { Provider } from "react-redux";
 import store from "../../reducers/store";
 import DropdownMenu from "./DropDownMenu";
 import "./Dashboard.css";
 import Card from "./card";
 import { useNavigate } from "react-router-dom";
+import PopupForm from './PopupForm';
+import React, { useState } from 'react';
+
+
 
 function Dashboard() {
   const navigate = useNavigate();
+  const [isFormOpen, setFormOpen] = useState(false);
 
-  const handleAddTrip = () => {
+
+    const handleAddTrip = () => {
     // TODO: Implement logic to handle adding a trip.json
+        setFormOpen(true);
     console.log("Add Trip button clicked!");
   };
+
+    const handleSubmit = (formData) => {
+        // Handle form submission logic here
+        console.log(formData);
+        setFormOpen(false);
+    };
+
+    const handleClose = () => {
+        setFormOpen(false);
+    };
 
   return (
     <div>
@@ -21,6 +38,9 @@ function Dashboard() {
       <div className="trip-button">
         <button onClick={handleAddTrip}>Add Trip</button>
       </div>
+        {isFormOpen && (
+            <PopupForm onSubmit={handleSubmit} onClose={handleClose} />
+        )}
       {/* Other components and content */}
       <div className="card-gallery">
         <Card />
