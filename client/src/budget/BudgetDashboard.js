1f684afae6769fa07ec19f91214e2555b0e336a9
jessaberry <shii@students.ubc.ca>
2023-07-06 02:22:52 -0700

    feat(budget): added trip costs raw, without charts-js-2

--- client/src/budget/BudgetDashboard.js
+++ client/src/budget/BudgetDashboard.js
@@ -1,17 +1,18 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import { ActivityVis } from "./Doughnut.js";
 import { DateVis } from "./Line.js";
-
-// https://apexcharts.com/javascript-chart-demos/dashboards/modern/
+import TripVis from "./Blob.js";
 
 export default function BudgetDashboard() {
   const experiences = useSelector((state) => state.exp.experiences);
   const expenses = useSelector((state) => state.exp.expenses);
-
+  const trips = useSelector((state) => state.trip.trips);
   return (
     <div>
       <h1>Budget</h1>
+      {/* BUDGET BY TRIP */}
+      <h3>Trip</h3>
+      <TripVis trips={trips} experiences={experiences} expenses={expenses} />
       {/* BUDGET BY ACTIVITY TYPE */}
       <h3>Doughnut</h3>
       <ActivityVis experiences={experiences} expenses={expenses} />
