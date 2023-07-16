89194432c7dd817937ac3f558abd9868eeaf46dc
jessaberry <shii@students.ubc.ca>
2023-07-16 02:34:55 -0700

    fix(filter): filtering trip by TripId causes 404

--- client/src/trip/components/Trip.js
+++ client/src/trip/components/Trip.js
@@ -1,6 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTripAsync, deleteTripAsync, getTripsAsync, filterTripAsync } from "../reducers/thunksTrip";
+import {
+  addTripAsync,
+  deleteTripAsync,
+  filterTripAsync,
+  getTripsAsync,
+} from "../reducers/thunksTrip";
 import { getExperiencesAsync } from "../../experience/reducers/thunksExperience";
 import { useNavigate, Link, Route, Routes, useParams } from "react-router-dom";
 import TripHandler from "./TripHandler";
@@ -8,7 +13,7 @@ import TripSingle from "./TripSingle";
 import React from "react";
 import { deleteExperienceAsync } from "../../experience/reducers/thunksExperience";
 import "./styles.css";
-import {PageLayout} from "../../content/template/page-layout.mjs";
+import { PageLayout } from "../../content/template/page-layout.mjs";
 
 export default function Trip() {
   const trips = useSelector((state) => state.trip.trips);
@@ -38,6 +43,12 @@ export default function Trip() {
     dispatch(deleteExperienceAsync(expID));
   };
 
+  const handleFilterTrip = (tripUUID) => {
+    const trip = trips.find((trip) => trip.TripId === tripUUID);
+    dispatch(filterTripAsync(tripUUID));
+    navigate(`/trips/${trip.TripId}`, { state: { tripUUID } });
+  };  
+
   const getExperiences = (tripID) => {
     return experiences
       .filter((exp) => exp.TripId === tripID)
@@ -46,52 +57,44 @@ export default function Trip() {
           new Date(a.StartingLocalDateTime) - new Date(b.StartingLocalDateTime)
       );
   };
-  console.log(trips);
+
   return (
-<PageLayout>
-    <div>
-      <h1>Trip Manager</h1>
-      <TripHandler
-        handleAddTrip={handleAddTrip}
-        handleAddExperience={handleAddExperience}
-      />
-      <div className="card-container">
-        {trips.map((trip) => (
-          <div className="trip-item" key={trip.TripId}> 
-            <h3 className="trip-title">TRIP - {trip.Title}</h3>
-            <p className="trip-info">TRIP id: {trip.TripId}</p>
-            <Link to={`/trips/${trip.TripId}`} onClick={() => dispatch(filterTripAsync(trip.TripId))}>
-              <button>View Details</button>
-            </Link>
-            <button onClick={() => handleDeleteTrip(trip)}>Delete</button>
-            <div className="experience-list"> 
-              <h4 className="experience-heading">Experiences:</h4>
-              <div className="experience-card-container"> 
-                {getExperiences(trip.TripId).map((experience) => (
-                  <div className="experience-card" key={experience.ExperienceId}> 
-                    <h5 className="experience-title">{experience.Title}</h5> 
-                  </div>
-                ))}
+    <PageLayout>
+      <div>
+        <h1>Trip Manager</h1>
+        <TripHandler
+          handleAddTrip={handleAddTrip}
+          handleAddExperience={handleAddExperience}
+        />
+        <div className="card-container">
+          {trips.map((trip) => (
+            <div className="trip-item" key={trip.TripId}>
+              <h3 className="trip-title">TRIP - {trip.Title}</h3>
+              <p className="trip-info">TRIP id: {trip.TripId}</p>
+              <Link to={`/trips/${trip.TripId}`}>
+                <button onClick={() => handleFilterTrip(trip.TripId)}>View Details</button>
+              </Link>
+              <button onClick={() => handleDeleteTrip(trip)}>Delete</button>
+              <div className="experience-list">
+                <h4 className="experience-heading">Experiences:</h4>
+                <div className="experience-card-container">
+                  {getExperiences(trip.TripId).map((experience) => (
+                    <div className="experience-card" key={experience.ExperienceId}>
+                      <h5 className="experience-title">{experience.Title}</h5>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <Routes>
+          <Route
+            path={`/trips/:tripId`}
+            element={<TripSingle />}
+          />
+        </Routes>
       </div>
-      <Routes>
-        <Route
-          path={`/trips/${tripId}`}
-          element={
-            <TripSingle
-              trips={trips}
-              experiences={getExperiences(tripId)}
-              handleAddExperience={handleAddExperience}
-              handleDeleteExperience={handleDeleteExperience}
-              handleDeleteTrip={handleDeleteTrip}
-            />
-          }
-        />
-      </Routes>
-    </div>
-</PageLayout>
+    </PageLayout>
   );
 }
