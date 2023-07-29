07804110e29a53a784a9b69e6f08691e32908d36
jessaberry <shii@students.ubc.ca>
2023-07-29 09:16:05 -0700

    fix(deploy): i need a drink

--- client/src/trip/components/TripExpViewer.js
+++ client/src/trip/components/TripExpViewer.js
@@ -6,15 +6,14 @@ const TripExpViewer = ({ trip, getExperiences, handleDeleteExperience }) => {
         {getExperiences(trip.TripId).map((experience) => (
           <div className="experience-card" key={experience.ExperienceId}>
             <h5 className="experience-title">{experience.Title}</h5>
-            {/* <p>Experience ID: {experience.ExperienceId}</p> */}
             <p>Notes: {experience.Description}</p>
             <p>Budget: ${experience.Cost}</p>
             <p>Public? {String(experience.IsPublic)}</p>
-            <button
-              onClick={() => handleDeleteExperience(experience.ExperienceId)}
-            >
-              Delete Experience
-            </button>
+            {handleDeleteExperience !== null && (
+              <button onClick={() => handleDeleteExperience(experience.ExperienceId)}>
+                Delete Experience
+              </button>
+            )}
           </div>
         ))}
       </div>
