43e7175bcfb7535839e5e286f412e8871c8e4ab0
ubc-eric.chau <eric.chau@ubc.ca>
2023-07-30 09:38:36 -0700

    updated flag css for trips

--- client/src/trip/components/TripHandler.js
+++ client/src/trip/components/TripHandler.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import TripForm from "./TripForm";
 import TripPublicViewer from "./TripPublicViewer";
 import TripPersonalViewer from "./TripPersonalViewer";
+import TripDetails from "./TripDetails";
+
 
 const TripHandler = ({
   trips,
@@ -15,48 +17,58 @@ const TripHandler = ({
   handleDeleteExperience,
   showTripDetails,
 }) => {
-  const [showInput, setShowInput] = useState(false);
+    const [showInput, setShowInput] = useState(false);
 
-  const handleShowTrip = () => {
-    setShowInput(!showInput);
-  };
+    const handleShowTrip = () => {
+        setShowInput(!showInput);
+    };
 
-  const handleAddNewTrip = (trip) => {
-    handleAddTrip(trip);
-    setShowInput(false);
-  };
+    const handleAddNewTrip = (trip) => {
+        handleAddTrip(trip);
+        setShowInput(false);
+    };
+    if (trips.length === 1) {
+        return (
+<>
+    <TripDetails trip={trips}/>
+</>
 
-  return (
-    <div>
-      <button
-        type="button"
-        onClick={handleShowTrip}
-        style={{ marginBottom: "30px" }}
-      >
-        {" "}
-        New Trip
-      </button>
-      {showInput && <TripForm handleAddTrip={handleAddNewTrip} />}
-      <h2>My trips</h2>
-      <TripPersonalViewer
-        trips={trips}
-        poi={poi}
-        visible={visible}
-        getExperiences={getExperiences}
-        showTripDetails={showTripDetails}
-        handleAddExperience={handleAddExperience}
-        handleDeleteExperience={handleDeleteExperience}
-        handleDeleteTrip={handleDeleteTrip}
-      />
-      <h2>Other people's public trips</h2>
-      <TripPublicViewer
-        publicTrips={publicTrips}
-        poi={poi}
-        visible={visible}
-        getExperiences={getExperiences}
-        showTripDetails={showTripDetails}
-      />
-    </div>
-  );
+        )
+    } else {
+
+        return (
+            <div>
+
+                <button
+                    type="button"
+                    onClick={handleShowTrip}
+                    style={{marginBottom: "30px"}}
+                >
+                    {" "}
+                    New Trip
+                </button>
+                {showInput && <TripForm handleAddTrip={handleAddNewTrip}/>}
+                <h2>My trips</h2>
+                <TripPersonalViewer
+                    trips={trips}
+                    poi={poi}
+                    visible={visible}
+                    getExperiences={getExperiences}
+                    showTripDetails={showTripDetails}
+                    handleAddExperience={handleAddExperience}
+                    handleDeleteExperience={handleDeleteExperience}
+                    handleDeleteTrip={handleDeleteTrip}
+                />
+                <h2>Other people's public trips</h2>
+                <TripPublicViewer
+                    publicTrips={publicTrips}
+                    poi={poi}
+                    visible={visible}
+                    getExperiences={getExperiences}
+                    showTripDetails={showTripDetails}
+                />
+            </div>
+        );
+    }
 };
 export default TripHandler;
